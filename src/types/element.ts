63a2d51003e124fad8e4ac52db7b7398317972ae
awxalbert <guangyu.zhong@airwallex.com>
2023-11-09 11:32:36 +0800

    feat: add function types for AlrwallexElement

--- src/types/element.ts
+++ src/types/element.ts
@@ -4,7 +4,28 @@ export interface BaseOptions {
 
 export interface AirwallexElement<Options extends BaseOptions = BaseOptions> {
   options: Options;
+  /**
+   * mount the element on DOM
+   * @param ele CSS selector or html element instance
+   */
   mount: (ele: string | HTMLElement) => void;
+  /**
+   * unmount the component from DOM tree
+   */
   unmount: () => void;
+  /**
+   * destroy the element instance
+   */
+  destroy: () => void;
+  /**
+   * listen to events triggered by elements
+   * @param eventName event name
+   * @param callback callback function
+   * @returns unique event id
+   */
   on: (eventName: string, callback: (data: any) => void) => number;
+  /**
+   * submit form and get final formValues
+   */
+  submit?: () => Promise<unknown>;
 }
